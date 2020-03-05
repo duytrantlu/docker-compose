28505c836736f7069bcf56431d406c75792a5d82
duytm2029 <tran.manh.duy@sun-asterisk.com>
2020-03-05 19:41:51 +0700

    first commit

---
+++ mongo-seeder/replicate.js
@@ -0,0 +1,8 @@
+rs.initiate( {
+    _id : "rs0",
+    members: [
+      { _id: 0, host: "mongo-primary:27017" },
+      { _id: 1, host: "mongo-secondary:27017" },
+      { _id: 2, host: "mongo-third:27017" },
+    ]
+  });
---
+++ mongo-seeder/seed.js
@@ -0,0 +1,16 @@
+const today = new Date();
+ 
+db.issue.insertMany([
+  {
+    "title": "Wehner and Sons Unbranded Wooden Hat Avon challenge",
+    "description": "Wehner and Sons Unbranded Wooden Hat Avon challenge programming function Manager deposit Handmade Digitized",
+    "created_at": today,
+    "updated_at": today
+  },
+  {
+    "title": "Luettgen, Kassulke and Welch Incredible Plastic Mouse interactive capacitor",
+    "description": "Luettgen, Kassulke and Welch Incredible Plastic Mouse interactive capacitor quantifying Supervisor Bike Customer",
+    "created_at": today,
+    "updated_at": today
+  }
+}
